a31807fda7c295bc0eb2b1845f3186c44bfa857d
avikaush <a.kaushik@live.in>
2015-07-12 03:36:29 -0700

    Added the new sorting code

--- Sorting.c
+++ Sorting.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 
-void main(){
-	int a[7] = { 3, 2, 7, 4, 10, 2, 44 };
-	int b[7] = { 3, 2, 7, 4, 10, 2, 44 };
-	
+void sort_ascending(int *a, int size){
+
 	int i, j;
 	int temp;
-	for (i = 0; i < 7; i++){
+	for (i = 0; i < size; i++){
 		for (j = 0; j <= i; j++){
 			if (a[i] < a[j]){
 				temp = a[j];
@@ -15,6 +13,13 @@ void main(){
 			}
 		}
 	}
+
+}
+
+void sort_descending(int *b, int size){
+
+	int i, j;
+	int temp;
 
 	for (i = 0; i < 7; i++){
 		for (j = 0; j <= i; j++){
@@ -25,6 +30,23 @@ void main(){
 			}
 		}
 	}
+
+}
+
+void main(){
+	int a[7] = { 3, 2, 7, 4, 10, 2, 44 };
+	int b[7] = { 3, 2, 7, 4, 10, 2, 44 };
+
+	int i;
+
+	int size = 7;
+	
+	sort_ascending(a, size);
+
+	sort_descending(b, size);
+
+
+	
 
 
 	printf("\n The Array in assending order is : \n ");
